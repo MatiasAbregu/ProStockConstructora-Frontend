dbf74517724a2384bcb5cc7206806ff1a0685bf0
MatiasAbregu <matiasabregu506@gmail.com>
2025-04-22 17:17:32 -0300

    Main Menu 50% responsive

--- src/components/ButtonMenu.jsx
+++ src/components/ButtonMenu.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import "../styles/ButtonMenu.css";
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 
 export const ButtonMenu = ({ url, img, children, desc }) => {
+ 
+  const MotionLink = motion(Link);
+ 
   return (
-    <Link to={url} className="btn-mio">
+    <MotionLink to={url} className="btn-mio"
+    initial={{opacity: 0, y: 10}} whileInView={{opacity: 1, y: 0, transition: {duration: 0.8}}} 
+     whileHover={{scale: 1.08, transition: {duration: 0.1}}} viewport={{once: false}}>
       <div className="containerImg">
         <img src={img} />
       </div>
@@ -12,6 +18,6 @@ export const ButtonMenu = ({ url, img, children, desc }) => {
         <h3>{children}</h3>
         <p>{desc}</p>
       </div>
-    </Link>
+    </MotionLink>
   );
 };
